eafac9288b91c90c7bf54ab9595fc6a46876cac1
ssokov094 <ssokov094@gmail.com>
2018-03-14 01:21:07 +0300

    added sources

---
+++ include/print.hpp
@@ -0,0 +1,7 @@
+cat > include/print.hpp <<EOF
+#include <string>
+#include <fstream>
+#include <iostream>
+
+void print(const std::string& text, std::ostream& out = std::cout);
+void print(const std::string& text, std::ofstream& out);
